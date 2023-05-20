b75c4710c09168801cf4b14afd11d533c34af1d6
infern <semenmochalov55@gmail.com>
2023-05-20 20:02:06 +0700

    все работает, но не с выключенным

--- app/src/main/java/ru/infern/taskalarm/AlarmService.kt
+++ app/src/main/java/ru/infern/taskalarm/AlarmService.kt
@@ -1,13 +1,22 @@
 package ru.infern.taskalarm
 
 import android.app.IntentService
+import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.os.IBinder
 import android.os.PowerManager
 
-class AlarmService : IntentService("AlarmService") {
-    override fun onHandleIntent(intent: Intent?) {
-        val broadcastIntent = Intent(applicationContext, AlarmReceiver::class.java)
-        sendBroadcast(broadcastIntent)
+class AlarmService : Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val taskIntent = Intent(applicationContext, TaskActivity::class.java)
+        taskIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(taskIntent)
+        return START_NOT_STICKY
     }
 }
